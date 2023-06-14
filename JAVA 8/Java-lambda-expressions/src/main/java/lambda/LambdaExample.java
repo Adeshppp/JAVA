46edab07d9d59c2dbae00902791781db47ac176e
Adesh <adeshp948@gmail.com>
2023-06-13 23:01:57 -0400

    Implementing lambda expressions in different ways.

--- JAVA 8/Java-lambda-expressions/src/main/java/lambda/LambdaExample.java
+++ JAVA 8/Java-lambda-expressions/src/main/java/lambda/LambdaExample.java
@@ -15,8 +15,10 @@ public class LambdaExample {
 //            System.out.println("Circle class : draw() method");
 //        }
 
+        System.out.println("================================ By using Lambda Expressions ================================");
+
         Shape rectangle = () -> System.out.println("Rectangle class draw method.");
-//        rectangle.draw();
+        rectangle.draw();
 
         Shape square = () -> System.out.println("Square class draw method.");
         square.draw();
@@ -24,7 +26,17 @@ public class LambdaExample {
         Shape circle = () -> System.out.println("Circle class draw method.");
         circle.draw();
 
+        System.out.println("================================ By passing Lambda Expressions as a parameter ================================");
+
         print(rectangle);
+        print(square);
+        print(circle);
+
+        System.out.println("================================ By calling Lambda Expressions as a parameter ================================");
+
+        print(() -> System.out.println("Rectangle class draw method."));
+        print(() -> System.out.println("Square class draw method."));
+        print(() -> System.out.println("Circle class draw method."));
 
     }
 
