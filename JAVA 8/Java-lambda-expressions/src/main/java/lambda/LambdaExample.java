44e8cd9afbf7e805ea1fcfeec17505de6ea7b536
Adesh <adeshp948@gmail.com>
2023-06-13 22:49:43 -0400

    Converted Object Oriented Prtogramming in Functional programming using Lambda expression.

--- JAVA 8/Java-lambda-expressions/src/main/java/lambda/LambdaExample.java
+++ JAVA 8/Java-lambda-expressions/src/main/java/lambda/LambdaExample.java
@@ -1,7 +1,25 @@
 package lambda;
 
+
+// Functional interface
+interface Shape{
+    void draw();
+}
+
+class Rectangle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle class draw method.");
+    }
+}
+
+
+
+
 public class LambdaExample {
     public static void main(String[] args) {
-        
+        Shape rectangle = () -> System.out.println("rectangle class draw method.");
+        rectangle.draw();
     }
 }
