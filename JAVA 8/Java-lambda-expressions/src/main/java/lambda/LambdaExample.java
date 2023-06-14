0ff0d979f663ef8633d8413a41b60f18f1a28de2
Adesh <adeshp948@gmail.com>
2023-06-13 22:58:02 -0400

    Passing lambda expression as a method parameter.

--- JAVA 8/Java-lambda-expressions/src/main/java/lambda/LambdaExample.java
+++ JAVA 8/Java-lambda-expressions/src/main/java/lambda/LambdaExample.java
@@ -6,20 +6,29 @@ interface Shape{
     void draw();
 }
 
-class Rectangle implements Shape{
-
-    @Override
-    public void draw() {
-        System.out.println("Rectangle class draw method.");
-    }
-}
-
-
-
 
 public class LambdaExample {
     public static void main(String[] args) {
-        Shape rectangle = () -> System.out.println("rectangle class draw method.");
-        rectangle.draw();
+
+//        @Override
+//        public void draw() {
+//            System.out.println("Circle class : draw() method");
+//        }
+
+        Shape rectangle = () -> System.out.println("Rectangle class draw method.");
+//        rectangle.draw();
+
+        Shape square = () -> System.out.println("Square class draw method.");
+        square.draw();
+
+        Shape circle = () -> System.out.println("Circle class draw method.");
+        circle.draw();
+
+        print(rectangle);
+
+    }
+
+    private static void print(Shape shape){
+        shape.draw();
     }
 }
