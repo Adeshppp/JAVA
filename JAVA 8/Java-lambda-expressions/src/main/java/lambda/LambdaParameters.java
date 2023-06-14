951f3cd4470eefc71227b0226fba32179ed856f7
Adesh <adeshp948@gmail.com>
2023-06-13 23:34:50 -0400

    Implement lambda expression by using threads.

--- JAVA 8/Java-lambda-expressions/src/main/java/lambda/LambdaParameters.java
+++ JAVA 8/Java-lambda-expressions/src/main/java/lambda/LambdaParameters.java
@@ -6,19 +6,24 @@ interface Addable{
     int addition(int a, int b);
 }
 
-//class AddabelImpl implements Addable{
-//
-//    @Override
-//    public int addition(int a, int b) {
-//        return a+b;
-//    }
-//}
+class AddabelImpl implements Addable{
+
+    @Override
+    public int addition(int a, int b) {
+        return a+b;
+    }
+}
 
 public class LambdaParameters {
     public static void main(String[] args) {
+//
+//        Addable addition = (a,b) -> a+b;
+//        System.out.println(addition.addition(2,3));
 
-        Addable addition = (a,b) -> a+b;
-        System.out.println(addition.addition(2,3));
+        Addable abc = (a, b) -> {
+            int c = a+b-1;
+            return c;
+        };
 
     }
 }
