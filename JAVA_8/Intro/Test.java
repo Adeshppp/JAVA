cd8fd037f04271d3d8867ad4224fd9c85bde0825
Adesh <adeshp948@gmail.com>
2023-08-24 15:24:58 -0400

    Lambda expression

--- JAVA_8/Intro/Test.java
+++ JAVA_8/Intro/Test.java
@@ -1,12 +1,18 @@
 package JAVA_8.Intro;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class Test {
     public static void main(String[] args) {
+        Function<Integer,Integer> f = i -> i*i; // return value
+        System.out.println("Square of number is "+f.apply(3));
 
-        System.out.println(SquareIt(3));
+        Predicate<Integer> p = i -> i%2==0; // return true or false
+        System.out.println(p.test(4));
+
     }
 
-    public static int SquareIt(int i){
-        return i*i;
-    }
 }
+
+
---
+++ JAVA_8/Readme.md
@@ -0,0 +1,2 @@
+
+Link to notebook: https://njit0-my.sharepoint.com/:o:/g/personal/app27_njit_edu/EoNH8SPvou5Gvt9Gg7dzPxAB8x1VZN6XCQnOb7PDtLaMGw?e=fUymmp
