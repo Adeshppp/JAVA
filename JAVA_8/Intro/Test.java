69266052779009fee21d55ce98b647ade91dc4c0
Adesh <adeshp948@gmail.com>
2023-08-23 22:59:05 -0400

    Traditional way to create and run the function

---
+++ JAVA_8/Intro/Test.java
@@ -0,0 +1,12 @@
+package JAVA_8.Intro;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(SquareIt(3));
+    }
+
+    public static int SquareIt(int i){
+        return i*i;
+    }
+}
