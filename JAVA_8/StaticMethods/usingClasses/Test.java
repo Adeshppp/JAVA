4c4b2d544e354352c45d0ec5fcc4b10a58fddaa3
Adesh <adeshp948@gmail.com>
2023-08-26 22:12:19 -0400

    Static method inside interfaces.

---
+++ JAVA_8/StaticMethods/usingClasses/Test.java
@@ -0,0 +1,7 @@
+package JAVA_8.StaticMethods.usingClasses;
+
+public class Test implements Interf{
+    public static void main(String[] args) {
+        Interf.m1();
+    }
+}
---
+++ JAVA_8/StaticMethods/usingOnlyInterface/Interf.java
@@ -0,0 +1,7 @@
+package JAVA_8.StaticMethods.usingOnlyInterface;
+
+public interface Interf {
+    public static void main(String[] args) {
+        System.out.println("Interface static main method");
+    }
+}
