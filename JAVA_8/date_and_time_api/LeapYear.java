b4b588297e4076d9ad423456511639f47f778500
Adesh <adeshpadwal21@gmail.com>
2023-09-10 05:11:12 -0400

    Optional class

--- JAVA_8/date_and_time_api/LeapYear.java
+++ JAVA_8/date_and_time_api/LeapYear.java
@@ -6,14 +6,10 @@ import java.util.Scanner;
 public class LeapYear {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-
         System.out.print("Enter year : ");
         int i = sc.nextInt();
         Year year = Year.of(i);
         if(year.isLeap()) System.out.printf("%d is a leap year.",i);
         else System.out.printf("%d is not a leap year",i);
-            System.out.println();
-        }
     }
 }
